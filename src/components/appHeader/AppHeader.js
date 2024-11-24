3c821741f8ccfdce3a6fbcc418e9e400f828349f
Pavel <krobynetspavel@gmail.com>
2024-11-24 12:46:25 +0200

    fixed single cahr header

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,22 +1,38 @@
-import { Link , NavLink} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./appHeader.scss";
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <Link to="/">
+        <Link to="/react-marvel-proj/">
           <span>Marvel</span> information portal
         </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <NavLink end style={({isActive}) => ({color: isActive ? "#9F0013" : "#232222"})} to="/">Characters</NavLink>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9F0013" : "#232222",
+              })}
+              to="/react-marvel-proj/"
+            >
+              Characters
+            </NavLink>
           </li>
           /
           <li>
-            <NavLink end style={({isActive}) => ({color: isActive ? "#9F0013" : "#232222"})} to="/comics">Comics</NavLink>
+            <NavLink
+              end
+              style={({ isActive }) => ({
+                color: isActive ? "#9F0013" : "#232222",
+              })}
+              to="/comics"
+            >
+              Comics
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/components/infoHandler/InfoHandler.js
+++ src/components/infoHandler/InfoHandler.js
@@ -3,14 +3,13 @@ import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import MarvelService from "../../services/MarvelService";
 import AppBaner from "../appBanner/AppBanner";
-import setContent from "../../utils/setContent"
+import setContent from "../../utils/setContent";
 
 export default function InfoHandler(props) {
   const { itemId } = useParams();
   const [item, setItem] = useState(null);
 
-  const {getComic, getCharacter,process, setProcess } =
-    MarvelService();
+  const { getComic, getCharacter, process, setProcess } = MarvelService();
 
   useEffect(() => {
     updateItem();
@@ -19,12 +18,12 @@ export default function InfoHandler(props) {
   const updateItem = () => {
     props.char === "char"
       ? getCharacter(itemId).then(setItem).then(setProcess("confirmed"))
-      : getComic(itemId).then(setItem).then(setProcess("confirmed"))
+      : getComic(itemId).then(setItem).then(setProcess("confirmed"));
   };
 
   return (
     <>
-		<AppBaner/>
+      <AppBaner />
       {setContent(false, process, View, item)}
     </>
   );
@@ -32,28 +31,24 @@ export default function InfoHandler(props) {
 
 const View = ({ name, thumbnail, price, description, pageCount, language }) => {
   return (
-		<>
-		<Helmet>
-			<meta 
-			name="description"
-			content={`${name} page`}
-			/>
-			<title>{name}</title>
-		</Helmet>
-		<AppBaner />
-    <div className="single-comic">
-      <img src={thumbnail} alt={name} className="single-comic__img" />
-      <div className="single-comic__info">
-        <h2 className="single-comic__name">{name}</h2>
-        <p className="single-comic__descr">{description}</p>
-        <p className="single-comic__descr">{pageCount}</p>
-        <p className="single-comic__descr">{language}</p>
-        <div className="single-comic__price">{price}</div>
+    <>
+      <Helmet>
+        <meta name="description" content={`${name} page`} />
+        <title>{name}</title>
+      </Helmet>
+      <div className="single-comic">
+        <img src={thumbnail} alt={name} className="single-comic__img" />
+        <div className="single-comic__info">
+          <h2 className="single-comic__name">{name}</h2>
+          <p className="single-comic__descr">{description}</p>
+          <p className="single-comic__descr">{pageCount}</p>
+          <p className="single-comic__descr">{language}</p>
+          <div className="single-comic__price">{price}</div>
+        </div>
+        <Link to={price ? "/comics" : "/"} className="single-comic__back">
+          Back to all
+        </Link>
       </div>
-      <Link to={price ? "/comics" : "/"} className="single-comic__back">
-        Back to all
-      </Link>
-    </div>
-		</>
+    </>
   );
 };
